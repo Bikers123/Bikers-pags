733f2acc3d3a9e90a448781c9e0cd6f5b9482fd5
FedeGIT-hash <romelcomparta09@gmail.com>
2026-05-11 16:48:18 -0700

    fotos arreglo

--- tesalia_social/urls.py
+++ tesalia_social/urls.py
@@ -10,3 +10,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+elif getattr(settings, "USE_LOCAL_MEDIA", False):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
